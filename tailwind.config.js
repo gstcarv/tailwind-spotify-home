eaca4c073568617c914e8e8aefda1992ddf1e70d
Gustavo Carvalho <gstcarv@gmail.com>
2022-10-05 17:36:26 -0300

    setup: extending tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,41 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            spacing: {
+                14: "3.5rem",
+                "125px": "125px",
+            },
+
+            colors: {
+                primary: {
+                    400: "#1ED760",
+                    500: "#18D760",
+                },
+                secondary: {
+                    500: "#2D46B9",
+                },
+            },
+
+            backgroundImage: (theme) => ({
+                "spotify-theme": "url('/img/bursts-tablet.svg')",
+                "spotify-theme-mobile": "url('/img/bursts-mobile.svg')",
+            }),
+
+            backgroundSize: {
+                "175%": "175%",
+                "195%": "195%",
+            },
+
+            backgroundPosition: {
+                banner: "46% 4%",
+                "banner-mobile": "top 25% center",
+            },
+
+            fontSize: {
+                "9xl": "9rem",
+            },
+        },
     },
     plugins: [],
 };
